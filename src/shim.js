2c424a7fb2b7b72d92720e71a00463fd07109f4e
DillonF <threeven@gmail.com>
2017-10-23 15:52:34 -0400

    added this.setState callback support

--- src/shim.js
+++ src/shim.js
@@ -32,7 +32,12 @@ function Enhancer(componentName, options) {
       }
 
       // override the setState method to use redux
-      setState(updater, callback) {
+      setState(updater, callback = null) {
+        // register the callback
+        if (callback && typeof callback === 'function') {
+          this._rrls_cb = callback
+        }
+
         // merge old state with state from updater
         let newState = {}
         if (typeof updater === 'function') {
@@ -51,6 +56,10 @@ function Enhancer(componentName, options) {
       componentWillReceiveProps(nextProps) {
         if (!isEqual(this.props[propName], nextProps[propName])) {
           this.state = nextProps[propName]
+          if (this._rrls_cb) {
+            _rrls_cb() // call callback
+            _rrls_cb = null // unregister callback
+          }
         }
 
         // hook is only present on super if it is defined in the wrapped component
