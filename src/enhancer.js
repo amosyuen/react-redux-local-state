072bcbe2d63a18f70de92af163521f972cdc0b0f
Amos Yuen <amosyuen@gmail.com>
2017-10-31 22:11:59 -0700

    Fix require lodash/isequal to lodash/isEqual

--- src/enhancer.js
+++ src/enhancer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import isEqual from 'lodash/isequal'
+import isEqual from 'lodash/isEqual'
 
 import { setComponentState } from './actions'
 
